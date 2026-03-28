acf739fd8de6148eac83e7742da55d0572e0a659
Shivam Anand <shivamanand9009@gmail.com>
2026-03-28 20:43:49 +0530

    remove unused variable

--- frontend/src/ui.js
+++ frontend/src/ui.js
@@ -36,17 +36,6 @@ const nodeTypes = {
 
 const snapGrid = [gridSize, gridSize];
 
-// ✅ Selector outside component
-const selector = (state) => ({
-  nodes: state.nodes,
-  edges: state.edges,
-  getNodeID: state.getNodeID,
-  addNode: state.addNode,
-  onNodesChange: state.onNodesChange,
-  onEdgesChange: state.onEdgesChange,
-  onConnect: state.onConnect,
-});
-
 export const PipelineUI = () => {
   const reactFlowWrapper = useRef(null);
   const [reactFlowInstance, setReactFlowInstance] = useState(null);
